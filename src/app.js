9a97e4fb31fc6d9a7db3ffde234a5ccad4a485bd
soojene <73275204+soojene@users.noreply.github.com>
2021-09-14 18:05:30 +0900

    before deploying

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(localsMiddleware);
 app.use(express.static("img"));
-app.use("/frontend", express.static("clients"));
+app.use("/soo", express.static("clients"));
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan("dev"));
 app.use(express.json());
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import app from './app';
 import "./db";
