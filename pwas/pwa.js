85310fae7da5ccc3c8d1bf660ea9602876f5b75d
soojene <73275204+soojene@users.noreply.github.com>
2021-10-25 16:02:13 +0900

    test

--- pwas/pwa.js
+++ pwas/pwa.js
@@ -1,11 +1,21 @@
-if('pwabuilder-sw'in navigator){
-   window.addEventListener("load", () => {
-      navigator.pwabuilder-sw.register('/pwabuilder-sw.js').then((reg)=> {
-         console.log("service workder", reg);
-      })
-   })
-}
+// if('pwabuilder-sw'in navigator){
+//    window.addEventListener("load", () => {
+//       navigator.pwabuilder-sw.register('/pwabuilder-sw.js').then((reg)=> {
+//          console.log("service workder", reg);
+//       })
+//    })
+// }
 
 // import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
 //    const el = document.createElement('pwa-update');
 //    document.body.appendChild(el);
+
+if ('serviceWorker' in navigator) {
+   try{
+    navigator.serviceWorker.register("sw.js");
+    console.log("SW registered");
+   }catch(err){
+    console.log("pwa service workder에러:", err);
+   }
+
+  }
