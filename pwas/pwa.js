f3dd57ae968f77413729eddcc7880952ff945b8e
soojene <73275204+soojene@users.noreply.github.com>
2021-10-25 15:44:14 +0900

    Update pwa.js

--- pwas/pwa.js
+++ pwas/pwa.js
@@ -1,3 +1,11 @@
-import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-   const el = document.createElement('pwa-update');
-   document.body.appendChild(el);
+if('pwabuilder-sw'in navigator){
+   window.addEventListener("load", () => {
+      navigator.pwabuilder-sw.register('/pwabuilder-sw.js').then((reg)=> {
+         console.log("service workder", reg);
+      })
+   })
+}
+
+// import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+//    const el = document.createElement('pwa-update');
+//    document.body.appendChild(el);
